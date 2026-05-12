5a5282c15a075ae1554663ea5648d5c8062d3a54
nimurr <nimurnerob404@gmail.com>
2026-05-12 08:58:59 +0600

    `Removed Subscribtion component and replaced it with UserSubscribtion component in page.js`

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -3,7 +3,7 @@ import DontMissOut from '@/Components/Home/DontMissOut';
 import EventsNearYou from '@/Components/Home/EventsNearYou';
 import ExplorebyCategory from '@/Components/Home/ExplorebyCategory';
 import Hero from '@/Components/Home/Hero';
-import Subscribtion from '@/Components/Home/Subscribtion';
+import Subscribtion from '@/Components/Home/UserSubscribtion';
 import TrendingNow from '@/Components/Home/TrendingNow';
 import Loading from '@/Components/others/Loading';
 import { useGetDemoDataQuery } from '@/redux/fetures/Demo/demoDataGet';
