dbde801d5de1d0108ed8af7b9291aa34472132b6
nimurr <nimurnerob404@gmail.com>
2026-05-16 15:05:45 +0600

    Add ProfileDetails and ProfileHero components for user profile page

---
+++ src/Components/profile/ProfileHero.js
@@ -0,0 +1,44 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import Header from '../Common/Header';
+
+const ProfileHero = () => {
+    const islands = ['All', 'New Providence', 'Grand Bahama', 'Abaco', 'Eleuthera', 'Exumas', 'Long Island'];
+    const [activeIsland, setActiveIsland] = useState('New Providence');
+    useEffect(() => {
+        const timer = setTimeout(() => setVideoExpanded(true), 1000);
+        return () => clearTimeout(timer);
+    }, []);
+
+
+    const [videoExpanded, setVideoExpanded] = useState(false);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setVideoExpanded(true);
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, []);
+    const txt = `transition-colors duration-1000 ${videoExpanded ? 'text-white' : ''}`;
+
+    return (
+        <div className=' overflow-hidden md:min-h-[60vh] min-h-[70vh] z-0'>
+            <Header />
+
+            {/* ── Background video — absolute stays inside Hero only ── */}
+            <div className={`absolute overflow-hidden md:max-h-[60vh] max-h-[70vh] rounded-b-2xl inset-0 z-0 transition-opacity duration-1000 ease-in-out
+                ${videoExpanded ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
+                {/* <video
+                    autoPlay muted loop playsInline
+                    src="/Images/Events/event_detials_hero.mp4"
+                    className="w-full h-full object-cover "
+                /> */}
+                <img className="w-full h-full object-cover " src="/Images/Events/Profile_Cover.jpg" alt="" />
+                <div className="absolute inset-0 bg-black/50" />
+            </div>
+
+
+        </div>
+    );
+};
+
+export default ProfileHero;
--- src/app/(main)/profile/page.js
+++ src/app/(main)/profile/page.js
@@ -1,9 +1,12 @@
+import ProfileDetails from '@/Components/profile/ProfileDetails';
+import ProfileHero from '@/Components/profile/ProfileHero';
 import React from 'react';
 
 const Page = () => {
     return (
         <div>
-            
+            <ProfileHero />
+            <ProfileDetails />
         </div>
     );
 }
