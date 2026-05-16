c0136247fcfc8e9e1e64973a3a9ed8c53e9025e3
nimurr <nimurnerob404@gmail.com>
2026-05-16 10:17:36 +0600

    Add DriveHero and DriveDetails components with video integration

---
+++ src/Components/Drive/DriveDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DriveDetails = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default DriveDetails;
---
+++ src/app/(main)/drive/page.js
@@ -0,0 +1,14 @@
+import DriveDetails from '@/Components/Drive/DriveDetails';
+import DriveHero from '@/Components/Drive/DriveHero';
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            <DriveHero />
+            <DriveDetails />
+        </div>
+    );
+}
+
+export default Page;
